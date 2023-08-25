7897e8fb300dc41fd7d8aa1dcdecc27c5a681b3c
sampark1234 <sampark1234@wavus.co.kr>
2023-08-25 12:26:45 +0900

    fix sql, persistence.xml for blog sample

--- src/main/resources/META-INF/sql/blog_web/blog_web_data.sql
+++ src/main/resources/META-INF/sql/blog_web/blog_web_data.sql
@@ -1,23 +1,23 @@
-insert into jpa.users(user_id, nickname, email, phone_number, age)
+insert into blog.users(user_id, nickname, email, phone_number, age)
 values (1, 'CodingToastBread', 'dailycode@velog.io', '010-0000-0000', 24),
        (2, 'CodingChicken', 'jonLee@velog.io', '010-1111-1111', 28)
 ;
 
-alter sequence jpa.users_id_seq restart with 3;
+alter sequence blog.users_id_seq restart with 3;
 
-select * from jpa.users;
+select * from blog.users;
 
-insert into jpa.blog(blog_id, blog_name, blog_create_date, user_id)
+insert into blog.blog(blog_id, blog_name, blog_create_date, user_id)
 values (1, 'DailyLifeCoding Blog', '2022-10-10', 1),
        (2, 'ToastBread''s Hobby Blog', '2023-01-01', 1),
        (3, 'Jon''s Tech Blog', '2021-12-01', 2),
        (4, 'Jon''s Gaming Blog', '2020-11-20', 2);
 
-alter sequence jpa.blog_id_seq restart with 5;
+alter sequence blog.blog_id_seq restart with 5;
 
-select * from jpa.blog;
+select * from blog.blog;
 
-insert into jpa.menu(menu_id, menu_name, menu_order, parent_menu_id, blog_id)
+insert into blog.menu(menu_id, menu_name, menu_order, parent_menu_id, blog_id)
 VALUES (1, 'programming', 0, null, 1),
        (2, 'C++', 0, 1, 1),
        (3, 'Python', 1, 1, 1),
@@ -26,11 +26,11 @@ VALUES (1, 'programming', 0, null, 1),
        (6, 'Operating System', 0, 5, 3),
        (7, 'Computer Structure', 1, 5, 3);
 
-alter sequence jpa.menu_id_seq restart with 8;
+alter sequence blog.menu_id_seq restart with 8;
 
-select * from jpa.menu;
+select * from blog.menu;
 
-insert into jpa.post(post_id, menu_id, subject, content, writer_id)
+insert into blog.post(post_id, menu_id, subject, content, writer_id)
 values (1, 2, 'What is C++?', 'C++ Content...', 1),
        (2, 3, 'What is Python?', 'Python Content...', 1),
        (3, 4, 'What is Java?', 'Java Content...', 1),
@@ -38,7 +38,37 @@ values (1, 2, 'What is C++?', 'C++ Content...', 1),
        (5, 6, 'Why Asynchronous Client Is Important?', 'Async Bla bla bla', 2),
        (6, 7, 'Why Do We Need To Learn Computer Science', 'Because...', 2);
 
-alter sequence jpa.post_id_seq restart with 7;
+alter sequence blog.post_id_seq restart with 7;
+
+insert into blog.tag(tag_id, tag_name)
+values (1, 'Programming'),
+       (2, 'CS');
+
+alter sequence blog.tag_id_seq restart with 3;
+
+insert into blog.post_tag(post_id, tag_id)
+values (1, 1),
+       (2, 1),
+       (3, 1),
+       (4, 2),
+       (5, 2),
+       (6, 2);
 
 
+insert into blog.comment (comment_id, post_id, content, writer_id)
+values (1, 1, 'C++ Reply...1', 2),
+       (2, 1, 'C++ Reply...2', 2),
+       (3, 2, 'Python Reply...1', 2),
+       (4, 2, 'Python Reply...2', 2),
+       (5, 3, 'Java Reply...1', 2),
+       (6, 3, 'Java Reply...2', 2);
 
+insert into blog.comment (comment_id, post_id, content, writer_id)
+values (7, 4, 'About Thread Reply...1', 2),
+       (8, 4, 'About Thread Reply...2', 2),
+       (9, 4, 'About Thread Reply...3', 2),
+       (10, 5, 'Why Async - Reply...1', 2),
+       (11, 5, 'Why Async - Reply...1', 2),
+       (12, 6, 'CS Reply...1', 2);
+
+alter sequence blog.comment_id_seq restart with 13;
--- src/main/resources/META-INF/sql/blog_web/blog_web_ddl.sql
+++ src/main/resources/META-INF/sql/blog_web/blog_web_ddl.sql
@@ -1,148 +1,157 @@
 set client_min_messages = warning;
 
-drop table if exists jpa.users cascade;
-drop table if exists jpa.blog cascade;
-drop table if exists jpa.menu cascade;
-drop table if exists jpa.post cascade;
-drop table if exists jpa.comment cascade;
-drop table if exists jpa.tag cascade;
-drop table if exists jpa.post_tag cascade;
+/* drop all table in blog schema */
 
-drop sequence if exists jpa.users_id_seq;
-drop sequence if exists jpa.blog_id_seq;
-drop sequence if exists jpa.menu_id_seq;
-drop sequence if exists jpa.post_id_seq;
-drop sequence if exists jpa.comment_id_seq;
-drop sequence if exists jpa.tag_id_seq;
-drop sequence if exists jpa.post_tag_id_seq;
+create schema if not exists blog;
+alter schema blog owner to postgres;
 
-create sequence jpa.users_id_seq;
-create sequence jpa.blog_id_seq;
-create sequence jpa.menu_id_seq;
-create sequence jpa.post_id_seq;
-create sequence jpa.comment_id_seq;
-create sequence jpa.tag_id_seq;
-create sequence jpa.post_tag_id_seq;
-
-comment on sequence jpa.users_id_seq is '사용자_테이블_시퀀스';
-comment on sequence jpa.blog_id_seq is '블로그_테이블_시퀀스';
-comment on sequence jpa.menu_id_seq is '메뉴_테이블_시퀀스';
-comment on sequence jpa.post_id_seq is '게시물_테이블_시퀀스';
-comment on sequence jpa.comment_id_seq is '댓글_테이블_시퀀스';
-comment on sequence jpa.tag_id_seq is '태그_테이블_시퀀스';
-comment on sequence jpa.post_tag_id_seq is '게시물_테그_테이블_시퀀스';
+DO $$
+DECLARE
+    r record;
+BEGIN
+    FOR r IN SELECT quote_ident(tablename) AS tablename, quote_ident(schemaname) AS schemaname FROM pg_tables WHERE schemaname = 'blog'
+        LOOP
+            RAISE INFO 'Dropping table if exists  %.%', r.schemaname, r.tablename;
+            EXECUTE format('DROP TABLE IF EXISTS %I.%I CASCADE', r.schemaname, r.tablename);
+        END LOOP;
+END$$;
 
 
-create table jpa.users (
-    user_id bigint not null default nextval('jpa.users_id_seq'),
+/* drop all sequence in blog schema */
+DO $$
+DECLARE
+    r record;
+BEGIN
+    FOR r IN select quote_ident(schemaname) AS schemaname, quote_ident(sequencename) as sequencename from pg_sequences where schemaname = 'blog'
+        LOOP
+            RAISE INFO 'Dropping table if exists %.%', r.schemaname, r.sequencename;
+            EXECUTE format('DROP SEQUENCE IF EXISTS %I.%I CASCADE', r.schemaname, r.sequencename);
+        END LOOP;
+END$$;
+
+create sequence blog.users_id_seq;
+create sequence blog.blog_id_seq;
+create sequence blog.menu_id_seq;
+create sequence blog.post_id_seq;
+create sequence blog.comment_id_seq;
+create sequence blog.tag_id_seq;
+create sequence blog.post_tag_id_seq;
+
+comment on sequence blog.users_id_seq is '사용자_테이블_시퀀스';
+comment on sequence blog.blog_id_seq is '블로그_테이블_시퀀스';
+comment on sequence blog.menu_id_seq is '메뉴_테이블_시퀀스';
+comment on sequence blog.post_id_seq is '게시물_테이블_시퀀스';
+comment on sequence blog.comment_id_seq is '댓글_테이블_시퀀스';
+comment on sequence blog.tag_id_seq is '태그_테이블_시퀀스';
+comment on sequence blog.post_tag_id_seq is '게시물_테그_테이블_시퀀스';
+
+
+create table blog.users (
+    user_id bigint not null default nextval('blog.users_id_seq'),
     nickname varchar(255) not null,
     email varchar(255) not null,
     phone_number varchar(30) not null,
     age smallint not null check ( age > 0 and age <= 140 ),
-    constraint jpa_user_pk primary key (user_id),
+    constraint blog_user_pk primary key (user_id),
     constraint users_nick_name_unique unique (nickname)
 );
 
-comment on table jpa.users is '블로그_웹사이트_사용자_테이블';
-comment on column jpa.users.user_id is '사용자_아이디';
-comment on column jpa.users.nickname is '사용자_닉네임';
-comment on column jpa.users.email is '사용자_이메일';
-comment on column jpa.users.phone_number is '사용자_전화번호';
-comment on column jpa.users.age is '사용자_나이';
+comment on table blog.users is '블로그_웹사이트_사용자_테이블';
+comment on column blog.users.user_id is '사용자_아이디';
+comment on column blog.users.nickname is '사용자_닉네임';
+comment on column blog.users.email is '사용자_이메일';
+comment on column blog.users.phone_number is '사용자_전화번호';
+comment on column blog.users.age is '사용자_나이';
 
-create table jpa.blog (
-    blog_id bigint not null default nextval('jpa.blog_id_seq'),
+create table blog.blog (
+    blog_id bigint not null default nextval('blog.blog_id_seq'),
     blog_name varchar(255) not null,
     blog_create_date date not null default current_date,
     user_id bigint not null,
-    constraint jpa_blog_pk primary key (blog_id),
-    constraint jpa_blog_to_user_fk foreign key (user_id) references jpa.users(user_id)
+    constraint blog_blog_pk primary key (blog_id),
+    constraint blog_blog_to_user_fk foreign key (user_id) references blog.users(user_id)
 );
 
-comment on table jpa.blog is '블로그_테이블';
-comment on column jpa.blog.blog_id is '블로그_식별_아이디';
-comment on column jpa.blog.blog_name is '블로그_명칭';
-comment on column jpa.blog.blog_create_date is '블로그_생성일자';
-comment on column jpa.blog.user_id is '블로그_생성자_아이디';
+comment on table blog.blog is '블로그_테이블';
+comment on column blog.blog.blog_id is '블로그_식별_아이디';
+comment on column blog.blog.blog_name is '블로그_명칭';
+comment on column blog.blog.blog_create_date is '블로그_생성일자';
+comment on column blog.blog.user_id is '블로그_생성자_아이디';
 
 
-create table jpa.menu (
-    menu_id bigint not null default nextval('jpa.menu_id_seq'),
+create table blog.menu (
+    menu_id bigint not null default nextval('blog.menu_id_seq'),
     menu_name varchar(255) not null,
     menu_order smallint default 0,
     parent_menu_id bigint,
     blog_id bigint not null,
-    constraint jpa_menu_id primary key (menu_id),
-    constraint jpa_menu_name_unique unique (menu_name),
-    constraint jpa_menu_to_blog_fk foreign key (blog_id) references jpa.blog(blog_id),
-    constraint jpa_self_join_fk foreign key (parent_menu_id) references jpa.menu(menu_id)
+    constraint blog_menu_id primary key (menu_id),
+    constraint blog_menu_name_unique unique (menu_name),
+    constraint blog_menu_to_blog_fk foreign key (blog_id) references blog.blog(blog_id),
+    constraint blog_self_join_fk foreign key (parent_menu_id) references blog.menu(menu_id)
 );
 
 
-comment on table jpa.menu is '메뉴_테이블';
-comment on column jpa.menu.menu_id is '메뉴_아이디';
-comment on column jpa.menu.menu_name is '메뉴_이름';
-comment on column jpa.menu.menu_order is '메뉴_표출_순서';
-comment on column jpa.menu.parent_menu_id is '자식_메뉴_아이디';
-comment on column jpa.menu.blog_id is '소속된_블로그_아이디';
+comment on table blog.menu is '메뉴_테이블';
+comment on column blog.menu.menu_id is '메뉴_아이디';
+comment on column blog.menu.menu_name is '메뉴_이름';
+comment on column blog.menu.menu_order is '메뉴_표출_순서';
+comment on column blog.menu.parent_menu_id is '자식_메뉴_아이디';
+comment on column blog.menu.blog_id is '소속된_블로그_아이디';
 
 
-create table jpa.post (
-    post_id bigint not null default nextval('jpa.post_id_seq'),
+create table blog.post (
+    post_id bigint not null default nextval('blog.post_id_seq'),
     menu_id bigint not null,
     subject varchar(255) not null,
     content varchar (1000) not null,
     writer_id bigint not null,
-    constraint jpa_post_pk primary key (post_id),
-    constraint jpa_post_to_menu_fk foreign key (menu_id) references jpa.menu(menu_id)
+    constraint blog_post_pk primary key (post_id),
+    constraint blog_post_to_menu_fk foreign key (menu_id) references blog.menu(menu_id)
 );
 
-comment on table jpa.post is '게시물_테이블';
-comment on column jpa.post.post_id is '게시물_아이디';
-comment on column jpa.post.menu_id is '소속_메뉴_아이디';
-comment on column jpa.post.subject is '제목';
-comment on column jpa.post.content is '내용';
-comment on column jpa.post.writer_id is '작성자_아이디';
+comment on table blog.post is '게시물_테이블';
+comment on column blog.post.post_id is '게시물_아이디';
+comment on column blog.post.menu_id is '소속_메뉴_아이디';
+comment on column blog.post.subject is '제목';
+comment on column blog.post.content is '내용';
+comment on column blog.post.writer_id is '작성자_아이디';
 
-create table jpa.comment (
-    comment_id bigint not null default nextval('jpa.comment_id_seq'),
+create table blog.comment (
+    comment_id bigint not null default nextval('blog.comment_id_seq'),
     post_id bigint not null,
     content varchar(500),
     writer_id bigint,
-    constraint jpa_comment_pk primary key (comment_id),
-    constraint jpa_comment_to_post_fk foreign key (post_id) references jpa.post(post_id)
+    constraint blog_comment_pk primary key (comment_id),
+    constraint blog_comment_to_post_fk foreign key (post_id) references blog.post(post_id)
 );
 
-comment on table jpa.comment is '댓글_테이블';
-comment on column jpa.comment.comment_id is '댓글_아이디';
-comment on column jpa.comment.post_id is '소속_게시물_아이디';
-comment on column jpa.comment.content is '내용';
-comment on column jpa.comment.writer_id is '작성자_아이디';
+comment on table blog.comment is '댓글_테이블';
+comment on column blog.comment.comment_id is '댓글_아이디';
+comment on column blog.comment.post_id is '소속_게시물_아이디';
+comment on column blog.comment.content is '내용';
+comment on column blog.comment.writer_id is '작성자_아이디';
 
 
-create table jpa.tag (
-    tag_id bigint not null default nextval('jpa.tag_id_seq'),
+create table blog.tag (
+    tag_id bigint not null default nextval('blog.tag_id_seq'),
     tag_name varchar(255) not null,
-    constraint jpa_tag_id primary key (tag_id)
+    constraint blog_tag_id primary key (tag_id)
 );
 
-comment on table jpa.tag is '태그_테이블';
-comment on column jpa.tag.tag_id is '태그_아이디';
-comment on column jpa.tag.tag_name is '태그_명칭';
+comment on table blog.tag is '태그_테이블';
+comment on column blog.tag.tag_id is '태그_아이디';
+comment on column blog.tag.tag_name is '태그_명칭';
 
-create table jpa.post_tag (
-    post_id bigint not null default nextval('jpa.post_tag_id_seq'),
+create table blog.post_tag (
+    post_id bigint not null default nextval('blog.post_tag_id_seq'),
     tag_id bigint not null,
-    constraint jpa_post_tag_middle_to_post_fk foreign key (post_id) references jpa.post(post_id),
-    constraint jpa_post_tag_middle_to_tag_id_fk foreign key (tag_id) references jpa.tag(tag_id)
+    constraint blog_post_tag_middle_to_post_fk foreign key (post_id) references blog.post(post_id),
+    constraint blog_post_tag_middle_to_tag_id_fk foreign key (tag_id) references blog.tag(tag_id)
 );
 
-comment on table jpa.post_tag is '게시물_태그_중간_테이블';
-comment on column jpa.post_tag.post_id is '게시물_아이디';
-comment on column jpa.post_tag.tag_id is '태그_아이디';
-
+comment on table blog.post_tag is '게시물_태그_중간_테이블';
+comment on column blog.post_tag.post_id is '게시물_아이디';
+comment on column blog.post_tag.tag_id is '태그_아이디';
 
 set client_min_messages = default;
-
-
-
